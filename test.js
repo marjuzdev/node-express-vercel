f528b897370921fb3bfa2be6c04bdbd8800dad8f
marioajuez <marioajuez@gmail.com>
2023-07-25 14:25:31 -0500

    new message

--- test.js
+++ test.js
@@ -8,7 +8,11 @@ router.get('/test', (req, res) => {
       console.log("call for webkook");
       console.log("more text");
 
-      // console.log('req', req.);
+      console.log('req.query', JSON.stringify(req.query));
+      // console.log('req.query', req);
+
+      const {Usuario,External_Id, id, Transaccion, FechaRegistro } = req.query;
+      console.log('Usuario', Usuario);
 
       res.json({
         status: '200',
